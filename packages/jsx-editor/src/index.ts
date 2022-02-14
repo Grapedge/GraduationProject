689b114c2d51bd0a6388a8ca4d606f04b129bec0
grapedge <grapedge@gmail.com>
2022-02-14 13:31:55 +0800

    chore(jsx-editor): add babel

--- packages/jsx-editor/src/index.ts
+++ packages/jsx-editor/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello from jsx editor')
+import template from '@babel/template'
+
+const ReactCode = `import react from 'react'
+
+export default function App() {
+  return <div>Hello, JSX Editor</div>
+}
+`
+
+const ast = template.ast(ReactCode, {
+  plugins: ['jsx'],
+})
+
+console.log('run in browser!')
+
+export default ast
