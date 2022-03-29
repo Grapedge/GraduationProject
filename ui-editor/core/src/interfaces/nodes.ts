b3c4d76c9103fafb00849c87d565463e7d1f4e91
grapedge <grapedge@gmail.com>
2022-03-29 22:19:30 +0800

    feat: 添加部分 Node 查询方法

--- ui-editor/core/src/interfaces/nodes.ts
+++ ui-editor/core/src/interfaces/nodes.ts
@@ -1,8 +1,35 @@
-import type { ASTRoot, JSXNode } from './ast'
+import type {
+  ASTRoot,
+  JSXAttribute,
+  JSXElement,
+  JSXExpressionContainer,
+  JSXFragment,
+  JSXNode,
+  JSXSpreadAttribute,
+} from './ast'
 import type { Path } from './editor'
 
 export type NodeId = string
 
+export enum NodeType {
+  ELEMENT = 'JSXElement',
+  FRAGMENT = 'JSXFragment',
+  EXPRESSION = 'JSXExpressionContainer',
+  TEXT = 'JSXText',
+}
+
+export type ElementProps = (JSXAttribute | JSXSpreadAttribute)[]
+
+export type ElementPropValue =
+  | string
+  | JSXElement
+  | JSXFragment
+  | JSXExpressionContainer
+  | null
+
 export interface NodeMethods {
-  get: (root: ASTRoot, path: Path) => JSXNode | undefined
+  get: (path: Path, root: ASTRoot) => JSXNode | undefined
+  getType: (node: JSXNode) => NodeType
+  getProps: (element: JSXElement) => ElementProps
+  getProp: (element: JSXElement, key: string) => ElementPropValue
 }
--- ui-editor/core/src/models/Node.ts
+++ ui-editor/core/src/models/Node.ts
@@ -1,12 +1,35 @@
-// // @ts-nocheck
-// import { NodeMethods } from '@/interfaces/nodes'
-// import { nanoid } from 'nanoid'
+// @ts-nocheck
+import { ASTRoot, JSXElement } from '@/interfaces/ast'
+import { Path } from '@/interfaces/editor'
+import { NodeMethods } from '@/interfaces/nodes'
+import {
+  compose,
+  find,
+  identity,
+  path,
+  prop,
+  propEq,
+  useWith,
+  when,
+} from 'ramda'
 
-// const Node: NodeMethods = {
-//   createNodeMeta: (path) => ({
-//     id: nanoid(),
-//     path,
-//   }),
-// }
+const get = path
+const getType = prop('type')
+const getProps = path(['openingElement', 'attributes'])
 
-// export default Node
+const getProp = useWith(
+  compose(
+    when(propEq('type', 'StringLiteral'), prop('value')),
+    find(propEq('name'))
+  ),
+  [getProps, identity]
+)
+
+const Node: NodeMethods = {
+  get,
+  getType,
+  getProps,
+  getProp,
+}
+
+export default Node
